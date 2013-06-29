bd3d8f45b3929ed16219c2d5ab75892df50d2076
Maru Newby <marun@redhat.com>
2013-06-29 03:05:19 +0000

    Remove provisioning from the default manifest.
     * Separating out provisioning of users, tenants and images leaves
       the door open for reuse of the tempest configuration by other
       provisioning schemes.
    
    Change-Id: I1878bd0b518d61c25a33ec329bcc436eb1163749

--- README.markdown
+++ README.markdown
@@ -3,9 +3,5 @@ Module for installing and configuring tempest.
 Tempest is the test suite that can be used to run integration
 tests on an installed openstack environment.
 
-This module installs an environment.
-
-Even after this script runs, it is still necessary to
-manually install an image, and insert that images id
-into /var/lib/tempest/etc/tempest.conf.
-
+This module assumes the provisioning of the initial OpenStack
+resources has been done beforehand.
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,6 @@
-
 # class for installing and configuring tempest
-# This manifest just sets up the basic config for
-# tempest. After it is applied, the following still needs
-# to be performed.
-#  - ensure that openstack::auth is also applied
-#  - bash /tmp/test_nova.sh (this will install an image)
-#  - capture the image name of the created image with `glance index`
-#  - fill in the XXXX in /var/lib/tempest/etc/tempest.conf with the image ID
-#  - run `nosetests temptest`
+#
+# The class checks out the tempest repo and sets the basic config.
 #
 # Note that only parameters for which values are provided will be
 # managed in tempest.conf.
@@ -24,7 +17,6 @@ class tempest(
   # Glance image config
   #
   $image_name                = 'cirros',
-  $image_source              = 'https://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-x86_64-disk.img',
 
   # tempest.conf parameters
   #
@@ -127,52 +119,19 @@ class tempest(
     'whitebox/db_uri':                   value => $whitebox_db_uri;
   }
 
-  keystone_tenant { $tenant_name:
-    ensure      => present,
-    enabled     => 'True',
-    description => 'admin tenant',
-  }
-  keystone_user { $username:
-    ensure      => present,
-    enabled     => 'True',
-    tenant      => $tenant_name,
-    password    => $password,
-  }
-
-  keystone_tenant { $alt_tenant_name:
-    ensure      => present,
-    enabled     => 'True',
-    description => 'admin tenant',
-  }
-  keystone_user { $alt_username:
-    ensure      => present,
-    enabled     => 'True',
-    tenant      => $alt_tenant_name,
-    password    => $alt_password,
-  }
-
-  # install an image to use for testing...
-  glance_image { $image_name:
-    ensure           => present,
-    is_public        => 'yes',
-    container_format => 'bare',
-    disk_format      => 'qcow2',
-    source           => $image_source,
-  }
-
   # retrieve the name of the glance image
   # and use it to set tempest.conf
   tempest_glance_id_setter { 'image_ref':
     ensure            => present,
     tempest_conf_path => $tempest_conf,
     image_name        => $image_name,
-    require => [File[$tempest_conf], Glance_image[$image_name]]
+    require           => File[$tempest_conf],
   }
   tempest_glance_id_setter { 'image_ref_alt':
     ensure            => present,
     tempest_conf_path => $tempest_conf,
     image_name        => $image_name,
-    require => [File[$tempest_conf], Glance_image[$image_name]]
+    require           => File[$tempest_conf],
   }
 
 }
