be999e2f1147e6c8063cdbfa7a031cf92112e226
Mathieu Gagné <mgagne@iweb.com>
2013-06-30 17:03:09 -0400

    Fail if osfamily is not supported
    * Refactor tempest::params class to better fit what's already done
      in other OpenStack Puppet modules.
    * Fix variable scoping of $::osfamily fact.
    
    Change-Id: Id05e7b5541fed61d51ac89dc4fe31ac84b503752

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,25 +1,30 @@
 class tempest::params {
 
-  if $osfamily == 'redhat' {
-    $dev_packages = [
-                     'python-devel',
-                     'libxslt-devel',
-                     'libxml2-devel',
-                     'openssl-devel',
-                     'mysql-devel',
-                     'postgresql-devel',
-                     'patch',
-                     'gcc',
-                     ]
-  } elsif $osfamily == 'debian' {
-    # FIXME - This list of packages should be updated to match what is
-    # specified for redhat.
-    $dev_packages = [
-                     'python-dev',
-                     'libxslt-dev',
-                     'libxml2-dev',
-                     'libssl-dev',
-                     ]
+  case $::osfamily {
+    'RedHat': {
+      $dev_packages = [
+                       'python-devel',
+                       'libxslt-devel',
+                       'libxml2-devel',
+                       'openssl-devel',
+                       'mysql-devel',
+                       'postgresql-devel',
+                       'patch',
+                       'gcc',
+                       ]
+    }
+    'Debian': {
+      # FIXME - This list of packages should be updated to match what is
+      # specified for redhat.
+      $dev_packages = [
+                       'python-dev',
+                       'libxslt-dev',
+                       'libxml2-dev',
+                       'libssl-dev',
+                       ]
+    }
+    default: {
+      fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}, module ${module_name} only support osfamily RedHat and Debian")
+    }
   }
-
 }
