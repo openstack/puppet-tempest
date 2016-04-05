74cda6a532463fe1a894b6bef853d81d53fad859
Matt Fischer <matt@mattfischer.com>
2016-04-05 10:51:17 -0600

    Correct comments for config class
    The config provider for tempest requires a path argument, unlike other
    config providers. This change notes that. Comment only change.
    
    Change-Id: I27838312cd5cfd0c3c7dc7f0b3a3bae9017d2179

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,16 +6,23 @@
 #
 # [*tempest_config*]
 #   (optional) Allow configuration of arbitrary Tempest configurations.
+#   Note: The tempest_config provider does not hard-code the path
+#   of the tempest config file so that tempest can be run out of temporary
+#   locations (i.e. /tmp/tempest). For this reason, you MUST pass "path"
+#   in this hash.
 #   The value is an hash of tempest_config resources. Example:
-#   { 'DEFAULT/foo' => { value => 'fooValue'},
-#     'DEFAULT/bar' => { value => 'barValue'}
+#   { 
+#     'DEFAULT/foo' => { path => '/tmp/tempest/etc/tempest.conf'; value => 'fooValue'},
+#     'DEFAULT/bar' => { path => '/tmp/tempest/etc/tempest.conf'; value => 'fooValue'},
 #   }
 #   In yaml format, Example:
 #   tempest_config:
 #     DEFAULT/foo:
 #       value: fooValue
+#       path: /tmp/tempest/etc/tempest.conf
 #     DEFAULT/bar:
 #       value: barValue
+#       path: /tmp/tempest/etc/tempest.conf
 #
 #   NOTE: The configuration MUST NOT be already handled by this module
 #   or Puppet catalog compilation will fail with duplicate resources.
