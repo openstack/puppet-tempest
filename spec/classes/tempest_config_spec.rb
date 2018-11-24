e7baa14889cf23c5e06c1f5571cfbc2f00650630
Tobias Urdin <tobias.urdin@binero.se>
2018-11-24 15:47:06 +0100

    Convert to rspec-puppet-facts
    Change-Id: I7f11d81b421c4e54ba3178bc586bd9f46e7bf9ad

--- spec/classes/tempest_config_spec.rb
+++ spec/classes/tempest_config_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe 'tempest::config' do
-
   let :params do
-    { :tempest_config => {
+    {
+      :tempest_config => {
         'DEFAULT/foo' => { 'value'  => 'fooValue' },
         'DEFAULT/bar' => { 'value'  => 'barValue' },
         'DEFAULT/baz' => { 'ensure' => 'absent' }
@@ -11,10 +11,23 @@ describe 'tempest::config' do
     }
   end
 
-  it 'configures arbitrary tempest configurations' do
-    is_expected.to contain_tempest_config('DEFAULT/foo').with_value('fooValue')
-    is_expected.to contain_tempest_config('DEFAULT/bar').with_value('barValue')
-    is_expected.to contain_tempest_config('DEFAULT/baz').with_ensure('absent')
+  shared_examples 'tempest::config' do
+    it {
+      is_expected.to contain_tempest_config('DEFAULT/foo').with_value('fooValue')
+      is_expected.to contain_tempest_config('DEFAULT/bar').with_value('barValue')
+      is_expected.to contain_tempest_config('DEFAULT/baz').with_ensure('absent')
+    }
   end
 
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      it_behaves_like 'tempest::config'
+    end
+  end
 end
