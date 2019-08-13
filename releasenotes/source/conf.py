0c43f799f73a0286d0c3147c16444f675b5ea60b
ZhongShengping <chdzsp@163.com>
2019-08-13 11:29:08 +0800

    Bump the openstackdocstheme extension to 1.20
    Some options are now automatically configured by the version 1.20[1]:
    - project
    - html_last_updated_fmt
    - latex_engine
    - latex_elements
    
    [1]https://docs.openstack.org/openstackdocstheme/latest/#using-the-theme
    
    Change-Id: If5525382b4b900af8a18eb37b9a8c3c1e4f9fb65

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -44,7 +44,6 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'puppet-tempest Release Notes'
 copyright = u'2017, Puppet OpenStack Developers'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -137,7 +136,6 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
@@ -181,17 +179,6 @@ htmlhelp_basename = 'puppet-tempestReleaseNotesdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
-
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
-
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
-}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
