e3aaa0af8c519c9fee52d6e0d9876de5af1be13b
Andreas Jaeger <aj@suse.com>
2020-06-02 15:03:29 +0200

    Switch to newer openstackdocstheme and reno versions
    Switch to openstackdocstheme 2.2.1 and reno 3.1.0 versions. Using
    these versions will allow especially:
    * Linking from HTML to PDF document
    * Allow parallel building of documents
    * Fix some rendering problems
    
    Update Sphinx version as well.
    
    Disable openstackdocs_auto_name to use 'project' variable as name.
    
    Change pygments_style to 'native' since old theme version always used
    'native' and the theme now respects the setting and using 'sphinx' can
    lead to some strange rendering.
    
    openstackdocstheme renames some variables, so follow the renames
    before the next release removes them. A couple of variables are also
    not needed anymore, remove them.
    
    See also
    http://lists.openstack.org/pipermail/openstack-discuss/2020-May/014971.html
    
    Change-Id: I60b031ce537e21ce4320aa4f3f60a7f4c75b19c7

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -85,7 +85,7 @@ exclude_patterns = []
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'native'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
@@ -249,6 +249,6 @@ texinfo_documents = [
 locale_dirs = ['locale/']
 
 # openstackdocstheme options
-repository_name = 'openstack/puppet-tempest'
-bug_project = 'puppet-tempest'
-bug_tag = ''
+openstackdocs_repo_name = 'openstack/puppet-tempest'
+openstackdocs_bug_project = 'puppet-tempest'
+openstackdocs_bug_tag = ''
